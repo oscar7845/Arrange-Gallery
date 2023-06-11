d287f5f9d5a4b4ec02f066df423e50b7a4baedd9
oscar7845 <oc59@njit.edu>
2023-06-11 10:38:20 +0200

    detection and recognition

--- utils/ai.py
+++ utils/ai.py
@@ -2,6 +2,9 @@ import face_recognition
 from . import file
 import pandas as pd
 import numpy as np
+import cv2
+import time
+from multiprocessing import Pool
 
 def get_known_face_encodings(df):
     known_face_names = []
@@ -14,7 +17,7 @@ def get_known_face_encodings(df):
 
 def detect_persons(df, tolerance=0.6):
     #
-    # Calculates matches between faces and updates dataframe with names
+    # Calculates matches between faces and updates DataFrame with names
     #
     unknown_counter=0
     for i, row in df.iterrows():
@@ -32,20 +35,71 @@ def detect_persons(df, tolerance=0.6):
             unknown_counter+=1
     return df
 
-def detect_faces(image_path):
-    # Load the image
+
+def single_process_detect_faces(image_path, rescale):
+    #
+    # Detects faces and face encoding using (HOG default) + Linear SVM face detection.
+    #
     image = face_recognition.load_image_file(image_path)
-
-    # Find all the faces in the image
-    face_locations = face_recognition.face_locations(image)
-
-    # Extract face encodings for all detected faces
+    if rescale is not None:
+        width,height,_ = image.shape
+        new_size = (int(width*rescale), int(height*rescale))
+        image = cv2.resize(image, new_size)
+    face_locations = face_recognition.face_locations(image) # model can be changed!
     face_encodings = face_recognition.face_encodings(image, face_locations)
 
-    return face_locations, face_encodings
+    return face_locations, face_encodings, image
+
+def detect_all_faces_in_album(path, multi_process=True, workers=8, show_images=False, images_show_timer=1, rescale=None, estimate_time=True):
+    if multi_process:
+        return multi_process_detect_all_faces_in_album(path, workers=workers)
+    else:
+        return single_process_detect_all_faces_in_album(path, workers=workers, show_images=show_images, images_show_timer=images_show_timer, rescale=rescale, estimate_time=estimate_time)
 
 
-def detect_all_faces_in_album(path):
+def multi_process_detect_faces(image_path):
+    #
+    # Detects faces and face encoding using (HOG default) + Linear SVM face detection.
+    #
+    image = face_recognition.load_image_file(image_path)
+    face_locations = face_recognition.face_locations(image) # model can be changed! cnn is CUDA accelerated!
+    face_encodings = face_recognition.face_encodings(image, face_locations)
+
+    df_detection_column_names = ["image_path", "box", "face_encoding", "id"]
+    df = pd.DataFrame(columns=df_detection_column_names)
+
+    for rect, encodings in zip(face_locations,face_encodings):
+        new_row = pd.DataFrame({"image_path": [image_path], "box": [rect], "face_encoding": [encodings],"id": [None]})
+        df = pd.concat([df,new_row], ignore_index=True)
+
+    print(f"Done with {image_path}")
+
+    return df
+
+def multi_process_detect_all_faces_in_album(path, workers=8):
+    #
+    # Loops over each image in path subdirectories and detects faces and calculates face encodings. Returns a pandas.DataFrame where each detected face is a row.
+    # Multiprocess variant.
+    #
+    image_paths = file.find_images(path)
+
+    pool = Pool(workers)
+
+    dfs = pool.map(multi_process_detect_faces, image_paths)
+    
+    df = pd.concat(dfs, ignore_index=True)
+
+    print("")
+    print(" ---- Statistics -----")
+    print(f"A total of {len(df)} faces were detected in album.")
+    print("")
+
+    pool.close()
+    pool.join()
+
+    return df 
+
+def single_process_detect_all_faces_in_album(path, workers=8, show_images=False, images_show_timer=1, rescale=None, estimate_time=True):
     #
     # Loops over each image in path subdirectories and detects faces and calculates face encodings. Returns a pandas.DataFrame where each detected face is a row.
     #
@@ -54,23 +108,39 @@ def detect_all_faces_in_album(path):
     df_detection_column_names = ["image_path", "box", "face_encoding", "id"]
     df = pd.DataFrame(columns=df_detection_column_names)
 
+
     for i,path in enumerate(image_paths):
+        if estimate_time:
+            start_time = time.time()
 
         print(f"On image {i+1}/{len(image_paths)}")
-        face_rects, face_encodings = detect_faces(path)
+        face_rects, face_encodings, image = single_process_detect_faces(path, rescale)
+
+        if show_images:
+            for top, right, bottom, left in face_rects:
+                cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            resized_image = cv2.resize(image, (500, 500))
+            cv2.imshow(path, resized_image)
+            cv2.waitKey(images_show_timer)
+            cv2.destroyAllWindows()
 
         print(f"In image {path} {len(face_rects)} faces detected.")
-
-        
+    
         for  rect, encodings in zip(face_rects,face_encodings):
             new_row = pd.DataFrame({"image_path": [path], "box": [rect], "face_encoding": [encodings],"id": [None]})
             df = pd.concat([df,new_row], ignore_index=True)
+        
+        if estimate_time:
+            end_time = time.time()
+            execution_time = (end_time - start_time)*(len(image_paths)-i+1)
+            execution_time_str = time.strftime('%H:%M:%S', time.gmtime(execution_time))
+
+            print(f" Estimated Time Left : {execution_time_str}")
     
     print("")
     print(" ---- Statistics -----")
     print(f"A total of {len(df)} faces were detected in album.")
-    print("Showing DataFrame:")
-    print(df)
     print("")
 
     return df 
