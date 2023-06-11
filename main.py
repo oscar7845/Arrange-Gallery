553e0864605ae16971fed1167ddaee731194ad6c
oscar7845 <oc59@njit.edu>
2023-06-11 11:17:58 +0200

    last edit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from utils import ai
 from utils import db
-from utils import file
 import os
 from tabulate import tabulate
 
@@ -10,8 +9,8 @@ if __name__ == "__main__":
     # Face recognition and so on.
 
     path = "./images/" 
-    df = ai.detect_all_faces_in_album(path, workers=8) # TODO: add checkpointing, smaller dataset
-    df = ai.detect_persons(df, tolerance=0.6) # TODO: add checkpointing
+    df = ai.detect_all_faces_in_album(path, workers=8, checkpoint_interval=50)
+    df = ai.detect_persons(df, tolerance=0.6, checkpoint_interval=50)
 
     # Save to CSV file safe
     store_csv_path = './data/tmp/tmp.csv'
