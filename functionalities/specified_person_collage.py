e4d3f96bd7b7fcd8288ae6deec8835d27f44e14f
oscar7845 <oc59@njit.edu>
2023-06-13 17:39:46 +0200

    generate collage

--- functionalities/specified_person_collage.py
+++ functionalities/specified_person_collage.py
@@ -3,7 +3,11 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+import shutil
+from utils import db
 from utils import file
+from utils import run
+from utils import ai
 import os
 from pyfiglet import Figlet
 
@@ -14,3 +18,41 @@ if __name__ == "__main__":
 
     album_path = "./images/"
     target_path = "./target/"
+
+    backup_checkpoints = True
+    backup_folder = "./data/backups/"
+
+    backup_csv = True
+    csv_storage_path = "./data/tmp/fr_db.csv"
+
+    load_df = False
+    df_path = "./data/tmp/fr_db.csv"
+
+    if not load_df:
+        checkpoint_path1 = "./data/tmp/detect_faces_checkpoint.pkl"
+        checkpoint_path2 = "./data/tmp/compare_person_checkpoint.pkl"
+
+        df = run.face_recognition_on_album(
+            album_path,
+            workers=8,
+            tolerance=0.6,
+            checkpoint_interval=100,
+            backup_checkpoints=backup_checkpoints,
+            backup_folder=backup_folder,
+            checkpoint_path1=checkpoint_path1,
+            checkpoint_path2=checkpoint_path2,
+        )
+
+        file.save_csv(csv_storage_path, df)
+
+        if backup_csv:
+            file.backup(csv_storage_path, backup_folder)
+
+    else:
+        df = db.load_dataframe(df_path)
+
+    # Pretty print (DataFrame):
+    # print(tabulate(df, headers='keys', tablefmt='psql'))
+    print(df)
+
+    collage = ai.create_face_collage(df,["Unknown11"],target_path, (1920,1080))
