a79bde65f3af626e1fe37bcaf6a69a579175ad55
oscar7845 <oc59@njit.edu>
2023-06-20 19:26:45 +0200

    entrypoint commands docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$1" == "--slideshow" ]; then
+  echo "Executing the slideshow Python file..."
+  python3 /project6/features/create_slideshow_from_album.py
+elif [ "$1" == "--collage" ]; then
+  echo "Executing the collage Python file..."
+  python3 /project6/features/create_collage_for_specified_person.py
+elif [ "$1" == "--image-resize" ]; then
+  echo "Executing the Resize Image Python file..."
+  python3 /project6/features/resize_all_images.py
+elif [ "$1" == "--remove-duplicates" ]; then
+  echo "Executing the Remove Duplicate Images Python file..."
+  python3 /project6/features/remove_duplicate_images.py
+elif [ "$1" == "--detect-duplicates" ]; then
+  echo "Executing the Detect Duplicate Images Python file..."
+  python3 /project6/features/detect_duplicate_images.py
+elif [ "$1" == "--pytest" ]; then
+  echo "Executing pytest..."
+  pytest /project6/tests
+else
+  echo "No argument provided, executing the main Python file..."
+  python3 /project6/main.py
+fi
