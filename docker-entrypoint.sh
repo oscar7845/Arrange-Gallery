030fea1aa9fa33ec9ec18526f6987f2263758d96
oscar7845 <oc59@njit.edu>
2023-06-20 19:37:02 +0200

    fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,23 +1,24 @@
+#!/bin/bash
 
 if [ "$1" == "--slideshow" ]; then
   echo "Executing the slideshow Python file..."
-  python3 /project6/features/create_slideshow_from_album.py
+  python3 /app/features/album_slideshow.py
 elif [ "$1" == "--collage" ]; then
   echo "Executing the collage Python file..."
-  python3 /project6/features/create_collage_for_specified_person.py
+  python3 /app/features/specified_person_collage.py
 elif [ "$1" == "--image-resize" ]; then
   echo "Executing the Resize Image Python file..."
-  python3 /project6/features/resize_all_images.py
+  python3 /app/features/resize_images.py
 elif [ "$1" == "--remove-duplicates" ]; then
   echo "Executing the Remove Duplicate Images Python file..."
-  python3 /project6/features/remove_duplicate_images.py
+  python3 /app/features/remove_duplicates.py
 elif [ "$1" == "--detect-duplicates" ]; then
   echo "Executing the Detect Duplicate Images Python file..."
-  python3 /project6/features/detect_duplicate_images.py
+  python3 /app/features/detect_duplicates.py
 elif [ "$1" == "--pytest" ]; then
   echo "Executing pytest..."
-  pytest /project6/tests
+  pytest /app/tests
 else
   echo "No argument provided, executing the main Python file..."
-  python3 /project6/main.py
+  python3 /app/main.py
 fi
